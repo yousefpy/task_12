5f4e904d1e8d74d2e36975a19b8b17913c512d2f
Yousef Alsaeid <macshiba@mac.com>
2018-12-11 17:47:48 +0300

    Ready to deployment

--- task_12/settings.py
+++ task_12/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tnvhv(22@t1#1!f9$&6gl#0-&9(symuxb$=x_2h6zkg*tdi4iy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1' , '142.93.175.99']
 
 
 # Application definition
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'task_12.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'd1117387443c45c08d943a1bb3345b83',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
